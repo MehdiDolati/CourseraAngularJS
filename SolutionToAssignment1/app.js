10cab967610bdfd1070e11f4fa71b498f42ec1ea
Mehdi <mehdi.dolati@gmail.com>
2016-09-19 13:25:38 +1000

    Update app.js

--- SolutionToAssignment1/app.js
+++ SolutionToAssignment1/app.js
@@ -9,13 +9,15 @@ function DIController($scope, $filter) {
   $scope.insertedText = "";
   $scope.systemResponse = "";
   $scope.checkIfTooMuch = function() {
-    var count = $scope.insertedText.split(',').length;
-    if (count < 1)
+    if (!$scope.insertedText)
       $scope.systemResponse = "Please enter data first";
-    if (count < 4)
-      $scope.systemResponse = "Enjoy!";
-    else
-      $scope.systemResponse = "Too much!";
+    else {
+      var count = $scope.insertedText.split(',').length;
+      if (count < 4)
+        $scope.systemResponse = "Enjoy!";
+      else
+        $scope.systemResponse = "Too much!";
+    }
   }
 }
 })();
